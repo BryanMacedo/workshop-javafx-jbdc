299f60851d44101d0193a797da358e7e10e4907c
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-14 09:48:32 -0300

    MainViewController.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module org.example.workshopjavafxjbdc {
     requires javafx.fxml;
 
 
+    opens gui.controller to javafx.fxml;
+    exports gui.controller to javafx.fxml;
+
     opens org.example.workshopjavafxjbdc to javafx.fxml;
     exports org.example.workshopjavafxjbdc;
 }
