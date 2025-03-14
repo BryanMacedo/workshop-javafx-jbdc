d3fbfd4a887f4b379a6ac6222cffed65cc1656eb
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-14 11:44:34 -0300

    About view.

---
+++ src/main/java/guiClasses/util/Alerts.java
@@ -0,0 +1,14 @@
+package guiClasses.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+    public static void showAlert(String title, String header, String content, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.show();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,8 @@ module org.example.workshopjavafxjbdc {
     requires javafx.fxml;
 
 
-    opens gui.controller to javafx.fxml;
-    exports gui.controller to javafx.fxml;
+    opens guiClasses.controller to javafx.fxml;
+    exports guiClasses.controller to javafx.fxml;
 
     opens org.example.workshopjavafxjbdc to javafx.fxml;
     exports org.example.workshopjavafxjbdc;
