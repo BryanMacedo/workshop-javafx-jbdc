a37c9756c9f5b38330938c827ca13f9d60edac49
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-18 08:51:54 -0300

    DepartmentService.

--- src/main/java/guiClasses/controller/DepartmentListController.java
+++ src/main/java/guiClasses/controller/DepartmentListController.java
@@ -1,6 +1,9 @@
 package guiClasses.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -8,12 +11,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 import org.example.workshopjavafxjbdc.Main;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+    private DepartmentService service;
+
     @FXML
     private TableView<Department> tableViewDepartment;
 
@@ -26,11 +33,16 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
 
+    private ObservableList<Department> obsList;
+
     @FXML
     public void onBtNewAction(){
         System.out.println("onBtNewAction");
     }
 
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -44,6 +56,16 @@ public class DepartmentListController implements Initializable {
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateTableView(){
+        if (service == null){
+            throw new IllegalStateException("Service was null");
+        }
+
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableList(list);
+        tableViewDepartment.setItems(obsList);
     }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,12 @@ module org.example.workshopjavafxjbdc {
     opens guiClasses.controller to javafx.fxml;
     exports guiClasses.controller to javafx.fxml;
 
+    opens model.services to javafx.base;
+    exports model.services to javafx.base;
+
+    opens model.entities to javafx.base;
+    exports model.entities to javafx.base;
+
     opens org.example.workshopjavafxjbdc to javafx.fxml;
     exports org.example.workshopjavafxjbdc;
 }
